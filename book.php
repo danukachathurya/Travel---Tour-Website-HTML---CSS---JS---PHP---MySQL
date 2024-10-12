7546a4e671eb3dc12893d6b35deed00d52151c90
Danuka Chathurya <danukachathurya8@gmail.com>
2024-10-13 04:02:42 +0530

    complete booking page

--- book.php
+++ book.php
@@ -19,9 +19,60 @@
 
 <?php include 'header.php'; ?>
     
-<div class="heading" style="background:url(./images/header-bg-1.png) no-repeat">
+<div class="heading" style="background:url(./images/head-bg-3.png) no-repeat">
     <h1>book now</h1>
 </div>
+
+
+
+<!-- booking section starts -->
+
+<section class="booking">
+    <h1 class="heading-title">book your trip</h1>
+
+    <form action="book_form.php" method="post" class="book-form">
+        <div class="flex">
+            <div class="inputBox">
+                <span>name :</span>
+                <input type="text" placeholder="enter your name" name="name">
+            </div>
+            <div class="inputBox">
+                <span>email :</span>
+                <input type="email" placeholder="enter your email" name="email">
+            </div>
+            <div class="inputBox">
+                <span>phone :</span>
+                <input type="number" placeholder="enter your number" name="phone">
+            </div>
+            <div class="inputBox">
+                <span>address :</span>
+                <input type="text" placeholder="enter your address" name="address">
+            </div>
+            <div class="inputBox">
+                <span>where to :</span>
+                <input type="text" placeholder="place you want to visit" name="location">
+            </div>
+            <div class="inputBox">
+                <span>how many :</span>
+                <input type="number" placeholder="number of guests" name="guests">
+            </div>
+            <div class="inputBox">
+                <span>arrivals :</span>
+                <input type="date" name="arrivals">
+            </div>
+            <div class="inputBox">
+                <span>leaving :</span>
+                <input type="date" name="leaving">
+            </div>
+        </div>
+
+        <input type="submit" value="submit" class="btn" name="send">
+    </form>
+</section>
+
+<!-- booking section ends -->
+
+
 
 <!-- swiper js link -->
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
@@ -30,6 +81,6 @@
 <script src="js/script.js"></script>
  
 
-
+<?php include 'footer.php'; ?> 
 </body>
 </html>
